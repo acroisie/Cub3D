0c11851852a08c9c09133760603e9e9b4b092508
acroisie <acroisie@student.42lyon.fr>
2022-06-21 22:03:42 +0200

    WIP

--- srcs/raycasting.c
+++ srcs/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 10:05:20 by acroisie          #+#    #+#             */
-/*   Updated: 2022/06/21 21:37:27 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/06/21 22:03:15 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	ft_intersection(t_game *game, t_vect v1)
 {
+	dprintf(2, "Debug; v1.x = %d, v2.y = %d\n", (int)v1.x, (int)v1.y);
 	if (game->info.map[(int)v1.y][(int)v1.x] == 1)
 		return (1);
 	return (0);
@@ -25,16 +26,14 @@ void	ft_raycast(t_game *game)
 	// t_vect	v2;
 	double	temp;
 	double	dx;
-	int		obstacle;
-	int		i;
+	// int		obstacle;
 
-	obstacle = 0;
-	i = 0;
+	// obstacle = 0;
 	v1.x = game->info.pos_x;
 	v1.y = game->info.pos_y;
 	v1.lenght = 0;
-	while (!obstacle)
-	{
+	// while (!obstacle)
+	// {
 		if (game->info.orientation < M_PI_2 || \
 		game->info.orientation > 3 * M_PI_2)
 			dx = 1 - fmod(v1.x, 1);
@@ -42,10 +41,10 @@ void	ft_raycast(t_game *game)
 			dx = fmod(v1.x, 1);
 		v1.x += dx;
 		temp = dx / cos(game->info.orientation);
-		v1.lenght += temp;
-		v1.y += dx * sin(M_PI_2 - game->info.orientation);
-		obstacle = ft_intersection(game, v1);
-		dprintf(2, "count %d\n", i++);
-	}
+		v1.lenght = temp;
+		printf("Debug lenght; %f\n", v1.lenght);
+		v1.y += 0;
+	// 	obstacle = ft_intersection(game, v1);
+	// }
 	ft_draw_line(game, v1.lenght);
 }
