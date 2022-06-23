84c609ed25aebd2330b4d99c54aaddb82c462f38
Arnaud <acroisie@student.42lyon.fr>
2022-06-23 15:45:22 +0200

    add_colision

--- srcs/hooks_utils.c
+++ srcs/hooks_utils.c
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 14:55:42 by acroisie          #+#    #+#             */
-/*   Updated: 2022/06/20 15:46:36 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/06/23 15:44:31 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,46 @@
 
 void	forward(t_game *game)
 {
-	game->info.pos_x += cos(game->info.orientation) / 5;
-	game->info.pos_y += sin(game->info.orientation) / 5;
-	ft_display_map(game);
+	if (game->info.map[(int)(game->info.pos_y + sin(game->info.orientation) \
+	/ 5)][(int)(game->info.pos_x + cos(game->info.orientation) / 5)] != '1')
+	{
+		game->info.pos_x += cos(game->info.orientation) / 5;
+		game->info.pos_y += sin(game->info.orientation) / 5;
+		ft_display_map(game);
+	}
 }
 
 void	back(t_game *game)
 {
-	game->info.pos_x -= cos(game->info.orientation) / 5;
-	game->info.pos_y -= sin(game->info.orientation) / 5;
-	ft_display_map(game);
+	if (game->info.map[(int)(game->info.pos_y - sin(game->info.orientation) \
+	/ 5)][(int)(game->info.pos_x - cos(game->info.orientation) / 5)] != '1')
+	{
+		game->info.pos_x -= cos(game->info.orientation) / 5;
+		game->info.pos_y -= sin(game->info.orientation) / 5;
+		ft_display_map(game);
+	}
 }
 
 void	left(t_game *game)
 {
-	game->info.pos_x -= cos(game->info.orientation + M_PI_2) / 5;
-	game->info.pos_y -= sin(game->info.orientation + M_PI_2) / 5;
-	ft_display_map(game);
+	if (game->info.map[(int)(game->info.pos_y - sin(game->info.orientation \
+	+ M_PI_2) / 5)][(int)(game->info.pos_x - cos(game->info.orientation + \
+	M_PI_2) / 5)] != '1')
+	{
+		game->info.pos_x -= cos(game->info.orientation + M_PI_2) / 5;
+		game->info.pos_y -= sin(game->info.orientation + M_PI_2) / 5;
+		ft_display_map(game);
+	}
 }
 
 void	right(t_game *game)
 {
-	game->info.pos_x += cos(game->info.orientation + M_PI_2) / 5;
-	game->info.pos_y += sin(game->info.orientation + M_PI_2) / 5;
-	ft_display_map(game);
+	if (game->info.map[(int)(game->info.pos_y + sin(game->info.orientation \
+	+ M_PI_2) / 5)][(int)(game->info.pos_x + cos(game->info.orientation + \
+	M_PI_2) / 5)] != '1')
+	{
+		game->info.pos_x += cos(game->info.orientation + M_PI_2) / 5;
+		game->info.pos_y += sin(game->info.orientation + M_PI_2) / 5;
+		ft_display_map(game);
+	}
 }
